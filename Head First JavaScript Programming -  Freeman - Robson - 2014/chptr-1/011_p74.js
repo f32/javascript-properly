1d3f62fcb5262255465dc8dd9621eb1dd219bcc9
f32 <ric.bbrown@yandex.ru>
2015-05-27 12:22:03 +0300

    Exercise from this page is done

---
+++ Head First JavaScript Programming -  Freeman - Robson - 2014/chptr-1/011_p74.js
@@ -0,0 +1,26 @@
+// решение всех ошибок _с исправлением оных_
+
+// Test or jokes
+var joke = "JavaScript walked into a bar....";
+var toldJoke = false;
+var punchline = "Better watch out for those semi-colons.";
+var entage = 20;
+var result;
+
+if (toldJoke == true) {
+  alert(ounchline);
+} else {
+  alert(joke);
+}
+
+
+// Movie Night
+var zipCode = 98104;
+var joesFavouriteMovie = "Forbidden Planet";
+var movieTickets = 9;
+
+if (movieTickets >= 9) {
+  alert("Too much!");
+} else {
+  alert("We're going top see " + joesFavouriteMovie);
+}
---
+++ Head First JavaScript Programming -  Freeman - Robson - 2014/chptr-1/012_p75.js
@@ -0,0 +1,31 @@
+// if temp = 10 ?
+var celsius2fahrenheit = (9 / 5) * temp + 32  // 50
+
+
+// if color = "pink" ?
+var paint1 = color == "orange";   // false
+// if color = "orange" ?
+var paint2 = color == "orange";   // true
+
+
+// if name = "Martha" ?
+var customer = name + ", " + "you've won!"; // "Martha, you've won!"
+
+
+// if youtLevel = 2 ?
+var grade1 = yourLevel > 5;  // false
+// if yourLevel = 5 ?
+var grade2 = yourLevel > 5; // false
+// if yourLevel = 7 ?
+var grade3 = yourLevel > 5; // true
+
+
+// if level = 5; points = 30000; bonus = 3300; ?
+var winAmount = (level * points) + bonus  // 153300
+
+
+// if color = "pink" ?
+var carColor = color != "orange"; // true
+
+
+var num = 1000 + "108"  // "1000108"
