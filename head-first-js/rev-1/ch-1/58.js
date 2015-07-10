e29429d42aa995cd49b97f712890fdc938dad288
f32 <ric.bbrown@yandex.ru>
2015-07-10 21:40:50 +0300

    Exercises' revision and/or examples from this page are done.

---
+++ head-first-js/rev-1/ch-1/58.js
@@ -0,0 +1,19 @@
+if (cashInWallet > 5) {
+  order = "Кутить так кутить: чизбургер, картошку и колу";
+} else {
+  order = "А я возьму стакан воды";
+}
+
+
+
+if (scoops < 3) {
+  alert("Ice cream is running low!");
+}
+
+
+
+if (scoops >= 5) {
+  alert("Eat faster, the ice cream is going to melt!");
+} else if (scoops < 3) {
+  alert("Ice cream is running low!");
+}
